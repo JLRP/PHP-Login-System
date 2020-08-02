f8191ad0acc66a9abebe102abdbc922581483ebe
Jorge Rosado <jrosado@genesis.local>
2020-08-02 11:01:56 -0400

    Added config; added login/req pages; added footer

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+
+  //Allow the config
+  define('__CONFIG__', true);
+  //Require the config
+  require_once "inc/config.php";
+
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -13,33 +22,18 @@
   <body>
 
     <div class="uk-section uk-container">
-        <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk.grid="">
-          <form class="uk-form-stacked js.login">
+        <?php
+          echo "Hello world. Today is: ";
+          echo date("Y m d");
+         ?>
 
-              <div class="uk-margin">
-                  <label class="uk-form-label" for="form-stacked-text">Email</label>
-                  <div class="uk-form-controls">
-                      <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@example.com">
-                  </div>
-              </div>
+         <p>
+           <a href="login.php">Login</a>
+           <a href="register.php">Register</a>
+         </p>
+    </div>
 
-              <div class="uk-margin">
-                  <label class="uk-form-label" for="form-stacked-text">Password</label>
-                  <div class="uk-form-controls">
-                      <input class="uk-input" id="form-stacked-text" type="password" required="required" placeholder="Your password">
-                  </div>
-              </div>
+    <?php require_once "inc/footer.php" ?>
 
-              <div class="uk-margin">
-                  <button class="uk.button uk-button-default" type="submit">Login</button>
-              </div>
-
-          </form>
-        </div>
-    </div.
-
-    <!-- UIkit JS -->
-    <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.5/dist/js/uikit.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.5/dist/js/uikit-icons.min.js"></script>
   </body>
 </html>
